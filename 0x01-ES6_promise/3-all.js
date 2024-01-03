3896a46b57d445d56e2aa323c61bffd1657c43b0
Initech <inimfonebong001@gmail.com>
2024-01-03 15:14:18 +0000

    Add task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  Promise.all([photo, user]).then((result) => {
+    const [photoresult, userresult] = result;
+    console.log(`${photoresult.body} ${userresult.firstName} ${userresult.lastName}`);
+  })
+    .catch(() => console.log('Signup system offline'));
+}
