bf39ddcaed77f448baa56dcccbe881244c6f6173
Initech <inimfonebong001@gmail.com>
2024-01-04 18:44:46 +0000

    Add task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a Number');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an Array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an Array of strings');
+    }
+    this._students = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
