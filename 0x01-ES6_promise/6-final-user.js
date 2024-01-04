97851a10f09d3be33416a6f028cf9b8fdff65efb
Initech <inimfonebong001@gmail.com>
2024-01-04 13:39:05 +0000

    linted

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => {
-      return result.map((r) => ({
-        status: r.status,
-        value: r.status === 'fulfilled' ? r.value : String(r.reason),
-      }));
-    });
+    .then((result) => result.map((r) => ({
+      status: r.status,
+      value: r.status === 'fulfilled' ? r.value : String(r.reason),
+    })));
 }
