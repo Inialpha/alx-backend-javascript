db531bddeac1e8df80637dcd30686d0582d786eb
Initech <inimfonebong001@gmail.com>
2024-01-05 17:58:08 +0000

    Add task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,36 @@
+export default class Car {
+  constructor (brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  set brand (value) {
+    this._brand = value;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Create = this.constructor[Symbol.species];
+    return new Create()
+  }
+}
