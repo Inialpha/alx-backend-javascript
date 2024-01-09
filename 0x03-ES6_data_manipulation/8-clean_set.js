22ff970c068620f23921d5e95579817d032b6467
Initech <inimfonebong001@gmail.com>
2024-01-09 10:56:12 +0000

    task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !(set instanceof Set) || (typeof startString !== 'string')) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
 
   const newString = [];
   for (const value of set.values()) {
-    if (value.startsWith(startString)) {
+    if (value && value.startsWith(startString)) {
       const subString = value.substring(startString.length);
       newString.push(subString);
     }
