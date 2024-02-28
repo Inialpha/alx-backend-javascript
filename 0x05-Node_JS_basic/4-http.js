06f01524fa6571f0e374d125423880cfe2c646bb
Initech <inimfonebong001@gmail.com>
2024-02-28 01:38:48 +0000

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.end('Hello Holberton School!');
+const app = http.createServer()
+app.on('request', (req, res) => {
+  const text = 'Hello Holberton School!'
+  res.setHeader('content-Type', 'text/plain')
+  res.setHaeder('content-Length', text.length)
+  res.write(Buffer, from(text))
 });
+
 
 app.listen(1245);
