c6941010d6402c54facbb87cb5d4a1bc20f28386
Initech <inimfonebong001@gmail.com>
2024-03-01 15:56:14 +0000

    api.test.js

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const request = require('request');
+
+
+describe('index', () => {
+  it('test index', () => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+    })
+  })
+
+  it('test /cart/:id/(//d+)', () => {
+    request.get('http://localhost:7865/cart/12', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 12');
+    })
+  })
+
+  it('test /cart/:id/(//d+)', () => {
+    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+      expect(res.statusCode).to.be.equal(404);
+    })
+  });
+})
+
+
