702b45afa0fd92e3d18a9896637890154ca508da
Initech <inimfonebong001@gmail.com>
2024-03-01 17:30:54 +0000

    async

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -3,48 +3,54 @@ const request = require('request');
 
 
 describe('index', () => {
-  it('test index', () => {
+  it('test index', (done) => {
     request.get('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
+      done()
     })
   })
 
-  it('test /cart/:id/(//d+)', () => {
+  it('test /cart/:id/(//d+)', (done) => {
     request.get('http://localhost:7865/cart/12', (err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 12');
+      done()
     })
   })
 
-  it('test /cart/:id/(//d+)', () => {
+  it('test /cart/:id/(//d+)', (done) => {
     request.get('http://localhost:7865/cart/hello', (err, res, body) => {
       expect(res.statusCode).to.be.equal(404);
+      done()
     })
   });
 
-  it('test /cart/:id/(//d+)', () => {
+  it('test /cart/:id/(//d+)', (done) => {
     request.get('http://localhost:7865/cart/abc123!', (err, res, body) => {
       expect(res.statusCode).to.be.equal(404);
+      done()
     })
   });
 
-  it('test /available_payments', () => {
+  it('test /login', (done) => {
+    request.post('http://localhost:7865/login', {json: {userName: 'Ini'}},  (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Ini');
+      done()
+    })
+  });
+
+  it('test /available_payments', (done) => {
     request.get('http://localhost:7865/available_payments',  (err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
       expect(JSON.parse(body)).to.be.deep.equal({
 	payment_methods: {
 	  credit_cards: true,
 	  paypal: false
 	}
       });
-      expect(res.statusCode).to.be.equal(200);
+      done()
     })
-  });
-
-
-  it('test /login', () => {
-    request.post('http://localhost:7865/login', {json: {userName: 'Ini'}},  (err, res, body) => {
-      expect(body).to.be.equal('Welcome Ini');
-      expect(res.statusCode).to.be.equal(200);        })
   });
 })
